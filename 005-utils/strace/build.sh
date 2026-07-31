44edc3d97e8327eadd19e25a7839653f35665531
Aromatic <symwww@outlook.com>
2026-07-31 08:06:50 +0800

    fix(utils): build strace against Linux 6.18 UAPI

--- 005-utils/strace/build.sh
+++ 005-utils/strace/build.sh
@@ -11,16 +11,25 @@ pkgname=strace
 pkgver=6.17
 depends=(glibc)
 builddepends=(linux-headers)
-makedepends=(gcc make)
+makedepends=(gcc make patch)
 
 prepare() {
     local archive="$downloaddir/strace-$pkgver.tar.xz"
     download "https://github.com/strace/strace/releases/download/v$pkgver/strace-$pkgver.tar.xz" "$archive"
     checksum sha256 0a7c7bedc7efc076f3242a0310af2ae63c292a36dd4236f079e88a93e98cb9c0 "$archive"
     extract "$archive" "$srcdir/strace"
+    input_file "$recipedir/patches/0001-preserve-bsearch-const-result.patch" \
+        "$srcdir/preserve-bsearch-const-result.patch"
+    input_file "$recipedir/patches/0002-linux-6.18-mnt-ns-fd.patch" \
+        "$srcdir/linux-6.18-mnt-ns-fd.patch"
+    input_file "$recipedir/patches/0003-linux-6.18-tee-uapi.patch" \
+        "$srcdir/linux-6.18-tee-uapi.patch"
 }
 
 build() {
+    patch -d "$srcdir/strace" -Np1 < "$srcdir/preserve-bsearch-const-result.patch"
+    patch -d "$srcdir/strace" -Np1 < "$srcdir/linux-6.18-mnt-ns-fd.patch"
+    patch -d "$srcdir/strace" -Np1 < "$srcdir/linux-6.18-tee-uapi.patch"
     cd "$builddir"
     target_env "$srcdir/strace/configure" --prefix=/usr --disable-static \
         --disable-mpers --without-libunwind --without-libdw
